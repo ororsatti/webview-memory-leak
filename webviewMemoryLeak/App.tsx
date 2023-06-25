b23cd677374c519db9e795922634157f7762d5be
Or Antonio Orsatti <antonio@10play.dev>
2023-06-25 10:09:51 +0300

    init

---
+++ webviewMemoryLeak/App.tsx
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <View
+      style={{
+        backgroundColor: 'white',
+        height: '100%',
+        width: '100%',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <Text>Test</Text>
+    </View>
+  );
+}
+
+export default App;
