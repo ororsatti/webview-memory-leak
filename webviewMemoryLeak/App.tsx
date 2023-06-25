4799a58e46a1d46ff7508bd3b5549cca31b79b32
Or Antonio Orsatti <antonio@10play.dev>
2023-06-25 10:34:58 +0300

    crash

--- webviewMemoryLeak/App.tsx
+++ webviewMemoryLeak/App.tsx
@@ -1,27 +1,59 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {
+  createNativeStackNavigator,
+  NativeStackScreenProps,
+} from '@react-navigation/native-stack';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import WebView from 'react-native-webview';
+import {ExampleHTML} from './exampleHTML';
+type RootStackParamList = {
+  Home: undefined;
+  WebviewTest: undefined;
+};
 
-import React from 'react';
-import {Text, View} from 'react-native';
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
-function App(): JSX.Element {
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingLeft: 20,
+    paddingRight: 20,
+    alignItems: 'center',
+  },
+});
+
+const HomeScreen = ({
+  navigation,
+}: NativeStackScreenProps<RootStackParamList, 'Home'>) => {
   return (
-    <View
-      style={{
-        backgroundColor: 'white',
-        height: '100%',
-        width: '100%',
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}>
-      <Text>Test</Text>
+    <View style={styles.container}>
+      <Button
+        title="Go to example webview"
+        onPress={() => navigation.navigate('WebviewTest')}
+      />
+      <Text>
+        (to see memory leak navigate between the pages multiple times)
+      </Text>
     </View>
   );
-}
+};
+const WebviewTest = () => {
+  return <WebView source={{html: ExampleHTML}} />;
+};
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'Welcome'}}
+        />
+        <Stack.Screen name="WebviewTest" component={WebviewTest} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default App;
